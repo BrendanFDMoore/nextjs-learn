59f944564b5bd90b7b1708018817371f50be8e3f
Brendan Moore <git@bfdmoore.com>
2020-10-12 21:46:45 -0400

    FIrst batch of changes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,7 +11,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <h1 className="title">
+          Go to <Link href="posts/first-post"><a>first post</a></Link>
         </h1>
 
         <p className="description">
